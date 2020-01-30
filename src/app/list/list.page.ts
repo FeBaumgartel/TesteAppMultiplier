6879a5e394f212080038a4624e8cab4df3f2e5c9
FelipeBaum <40307228+FelipeBaum@users.noreply.github.com>
2020-01-30 17:51:43 -0300

    15

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -11,17 +11,20 @@ import { SpinnerDialog } from '@ionic-native/spinner-dialog/ngx';
   styleUrls: ['list.page.scss']
 })
 export class ListPage implements OnInit {
+
   public produtos: Array<Produto> = [];
-  constructor(private ProdutoService: ProdutoServiceService, private SincronizacaoService: SincronizacaoServiceService, private router: Router, private spinnerDialog: SpinnerDialog) { }
+  private inicio;
+  private fim;
+  constructor(private ProdutoService: ProdutoServiceService, private SincronizacaoService: SincronizacaoServiceService, private router: Router, private spinnerDialog: SpinnerDialog) {
+
+  }
 
   async ionViewWillEnter() {
     this.spinnerDialog.show();
+    this.inicio = 0;
+    this.fim = 9;
     this.produtos = [];
-    this.ProdutoService.getProdutos()
-      .then(a => {
-        this.produtos = a;
-        this.spinnerDialog.hide();
-      });
+    this.addProdutos();
   }
   public async Sync() {
     this.spinnerDialog.show();
@@ -44,7 +47,29 @@ export class ListPage implements OnInit {
     this.router.navigate(['/criaProduto']);
   }
 
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      this.addProdutos();
+      event.target.complete();
+    }, 500);
+  }
 
+  async addProdutos() {
+    await this.ProdutoService.getProdutos()
+      .then(async(a) => {
+        console.log(this.inicio);
+        console.log(this.fim);
+        var inserir = a.slice(this.inicio, this.fim);
+        console.log(inserir);
+        for (let i = 0; i < inserir.length; i++) {
+          await this.produtos.push(inserir[i]);
+        }
+      });
+    this.inicio += 10;
+    this.fim += 10;
+    this.spinnerDialog.hide();
+  }
 
   ngOnInit() {
   }
