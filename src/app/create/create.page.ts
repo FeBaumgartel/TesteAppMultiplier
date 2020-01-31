23acddbb8479b09509d0f618199e5dce8bae8313
FelipeBaum <40307228+FelipeBaum@users.noreply.github.com>
2020-01-31 18:07:03 -0300

    17

--- src/app/create/create.page.ts
+++ src/app/create/create.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Produto } from '../models/produto';
 import { ProdutoServiceService } from '../produto-service.service';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create',
@@ -11,14 +12,53 @@ import { Router } from '@angular/router';
 export class CreatePage {
 
   protected produto: Produto = new Produto();
-
-  constructor(private ProdutoService: ProdutoServiceService, private router: Router) { }
+  constructor(private ProdutoService: ProdutoServiceService, private router: Router, private toastController: ToastController) { }
 
   public async createProduct() {
+    if (await this.validacao())
+      return;
+
     await this.ProdutoService.postProdutos(this.produto);
     this.produto.nome = null;
     this.produto.preco = null;
     this.produto.peso = null;
     this.router.navigate(['/listaProduto']);
+  }
+
+  private async validacao() {
+    var replace = / /gi
+    var nome = await this.produto.nome.replace(replace, "");
+    if (nome == null || nome == "" || nome == undefined) {
+      let toast = await this.toastController.create({
+        message: 'Nome não pode estar vazio ou inválido.',
+        duration: 2000
+      });
+      toast.present();
+      return true;
+    }
+    if (this.produto.preco == null || this.produto.preco == undefined) {
+      let toast = await this.toastController.create({
+        message: 'Preço não pode estar vazio ou inválido.',
+        duration: 2000
+      });
+      toast.present();
+      return true;
+    }
+    if (this.produto.peso == null || this.produto.peso == undefined) {
+      let toast = await this.toastController.create({
+        message: 'Peso não pode estar vazio ou inválido.',
+        duration: 2000
+      });
+      toast.present();
+      return true;
+    }
+    if (this.produto.nome.length > 50) {
+      let toast = await this.toastController.create({
+        message: 'Nome não pode ter mais de 50 caracteres.',
+        duration: 2000
+      });
+      toast.present();
+      return true;
+    }
   }
 }
--- src/app/update/update.page.ts
+++ src/app/update/update.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Produto } from '../models/produto';
 import { ProdutoServiceService } from '../produto-service.service';
-import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -13,9 +13,12 @@ import { ActivatedRoute } from '@angular/router';
 export class UpdatePage implements OnInit {
   protected produto: Produto = new Produto();
   id: string;
-  constructor(private ProdutoService: ProdutoServiceService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private ProdutoService: ProdutoServiceService, private router: Router, private route: ActivatedRoute, private toastController: ToastController) { }
 
   public async updateProduct() {
+    if (await this.validacao())
+      return;
+
     await this.ProdutoService.putProdutos(this.produto);
     this.produto.nome = null;
     this.produto.preco = null;
@@ -27,10 +30,47 @@ export class UpdatePage implements OnInit {
     this.id = this.route.snapshot.paramMap.get('id');
     await this.ProdutoService.getProdutoById(this.id)
       .then(a => {
-        this.produto=a;
+        this.produto = a;
       });
   }
-  
+
+  private async validacao() {
+    var replace = / /gi
+    var nome = await this.produto.nome.replace(replace, "");
+    if (nome == null || nome == "" || nome == undefined) {
+      let toast = await this.toastController.create({
+        message: 'Nome não pode estar vazio ou inválido.',
+        duration: 2000
+      });
+      toast.present();
+      return true;
+    }
+    if (this.produto.preco == null || this.produto.preco == undefined) {
+      let toast = await this.toastController.create({
+        message: 'Preço não pode estar vazio ou inválido.',
+        duration: 2000
+      });
+      toast.present();
+      return true;
+    }
+    if (this.produto.peso == null || this.produto.peso == undefined) {
+      let toast = await this.toastController.create({
+        message: 'Peso não pode estar vazio ou inválido.',
+        duration: 2000
+      });
+      toast.present();
+      return true;
+    }
+    if (this.produto.nome.length > 50) {
+      let toast = await this.toastController.create({
+        message: 'Nome não pode ter mais de 50 caracteres.',
+        duration: 2000
+      });
+      toast.present();
+      return true;
+    }
+  }
+
   ngOnInit() {
   }
 }
